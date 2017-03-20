d3265155e668e777f082a737dd640b3c6cccf17b
Matteo Autieri <matteoautieri@live.it>
2017-03-20 21:23:12 +0100

    added fade effect

--- src/script.lua
+++ src/script.lua
@@ -5,31 +5,27 @@
 --
 -- @author  TyKonKet
 -- @date 20/03/2017
-
 Helpers = {};
 Helpers.name = "Helpers";
-Helpers.debug = true;
 
 function Helpers:print(txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9)
-    if self.debug then
-        local args = {txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9};
-        for i, v in ipairs(args) do
-            if v then
-                print("[" .. self.name .. "] -> " .. tostring(v));
-            end
+    local args = {txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9};
+    for i, v in ipairs(args) do
+        if v then
+            print("[" .. self.name .. "] -> " .. tostring(v));
         end
     end
 end
 
 function Helpers:initialize(missionInfo, missionDynamicInfo, loadingScreen)
     self = Helpers;
-    self:print("initialize()");
+    -- Instance of the fade effect object
+    self.fadeEffect = FadeEffect:new({position = {x = 0.5, y = 0.5}, size = 0.03, shadow = true, shadowPosition = {x = 0.0025, y = 0.0035}, statesTime = {1, 2, 1}});
 end
 g_mpLoadingScreen.loadFunction = Utils.prependedFunction(g_mpLoadingScreen.loadFunction, Helpers.initialize);
 
 function Helpers:load(missionInfo, missionDynamicInfo, loadingScreen)
     self = Helpers;
-    self:print("load()");
     g_currentMission.loadMapFinished = Utils.appendedFunction(g_currentMission.loadMapFinished, self.loadMapFinished);
     g_currentMission.onStartMission = Utils.appendedFunction(g_currentMission.onStartMission, self.afterLoad);
     g_currentMission.missionInfo.saveToXML = Utils.appendedFunction(g_currentMission.missionInfo.saveToXML, self.saveSavegame);
@@ -37,38 +33,31 @@ end
 g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFunction, Helpers.load);
 
 function Helpers:loadMap(name)
-    self:print(("loadMap(name:%s)"):format(name));
-    if self.debug then
-        addConsoleCommand("AAAHelpersTestCommand", "", "TestCommand", self);
-    end
+    addConsoleCommand("AAAHelpersFadeEffect", "", "playFadeEffect", self);
     self:loadSavegame();
 end
 
 function Helpers:loadMapFinished()
     self = Helpers;
-    self:print("loadMapFinished()");
 end
 
 function Helpers:afterLoad()
     self = Helpers;
-    self:print("afterLoad");
 end
 
 function Helpers:loadSavegame()
-    self:print("loadSavegame()");
 end
 
 function Helpers:saveSavegame()
     self = Helpers;
-    self:print("saveSavegame()");
 end
 
 function Helpers:deleteMap()
-    self:print("deleteMap()");
 end
 
-function Helpers:TestCommand()
-    return "This is a test command";
+function Helpers:playFadeEffect()
+    -- Start/play of the fade effect object
+    self.fadeEffect:play("Test fade effect!");
 end
 
 function Helpers:keyEvent(unicode, sym, modifier, isDown)
@@ -78,9 +67,13 @@ function Helpers:mouseEvent(posX, posY, isDown, isUp, button)
 end
 
 function Helpers:update(dt)
+    -- Update of the fade effect object
+    self.fadeEffect:update(dt);
 end
 
 function Helpers:draw()
+    -- Draw of the fade effect object
+    self.fadeEffect:draw();
 end
 
 addModEventListener(Helpers)
